ef851a2441672b5c3e055aede89bb95ea40d76bd
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-09-16 19:09:53 -0600

    Some more devlops

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLeads } from "../../actions/leads";
 
 export class Leads extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Leads list</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Leads list</h1>
+      </div>
+    );
+  }
 }
 
-export default Leads
+const mapStateToProps = (state) => ({
+  leads: state.leads.leads,
+});
+
+export default connect(Leads);
