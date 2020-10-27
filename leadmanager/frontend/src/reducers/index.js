8111eeeee12edb9c419dd18b2343f640e8efb100
Daniel Limas <amedlimas97@gmail.com>
2020-10-27 15:23:00 -0700

    Done adding messages

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import leads from "./leads";
 import errors from './errors'
+import messages from './messages'
+
 // Lead Reducer
 export default combineReducers({
   leads,
-  errors
+  errors,
+  messages
 });
